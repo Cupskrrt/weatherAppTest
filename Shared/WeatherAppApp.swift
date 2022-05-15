d6f8b0dbb9a86ba3bb1021842c6e7eadd1ccfe69
Cupang5 <khanalfarizzi@hotmail.com>
2022-05-15 22:08:34 +0700

    Initial Commit

---
+++ Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by Luthfi Khan on 15/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
